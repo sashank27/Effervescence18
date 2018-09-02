cc5fd44dfda5ab67faca7be825a2715b6dbea48a
noble-gas <bansalakshit56@gmail.com>
2018-09-03 02:53:13 +0530

    first commit

---
+++ app/src/main/java/org/effervescence/app18/Effervescence18.kt
@@ -0,0 +1,13 @@
+package org.effervescence.app18
+
+import android.app.Application
+import android.widget.Toast
+
+class Effervescence18 : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        Toast.makeText(this, "ApplicationStarted", Toast.LENGTH_SHORT).show()
+    }
+}
---
+++ app/src/main/java/org/effervescence/app18/activities/MainActivity.kt
@@ -0,0 +1,13 @@
+package org.effervescence.app18.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import org.effervescence.app18.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
