9b5afcb32c60f0a1a31421735bccd9896ed763f0
Akshit Bansal <bansalakshit56@gmail.com>
2018-09-28 11:50:25 +0530

    UI for Home fragment is ready (#13)

--- app/src/main/java/org/effervescence/app18/fragments/HomeFragment.kt
+++ app/src/main/java/org/effervescence/app18/fragments/HomeFragment.kt
@@ -1,15 +1,13 @@
 package org.effervescence.app18.fragments
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.LinearSnapHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.fragment_home.*
-
 import org.effervescence.app18.R
 import org.effervescence.app18.adapters.BookmarksAdapter
 import org.effervescence.app18.adapters.UpcomingAdapter
@@ -33,37 +31,38 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         activity!!.title = "My Effe"
-        upcomingRecyclerView.layoutManager = LinearLayoutManager(context,
-                LinearLayoutManager.HORIZONTAL, false)
 
         val layoutManager = LinearLayoutManager(context)
         layoutManager.isAutoMeasureEnabled = true
 
-        bookmarksRecyclerView.layoutManager = LinearLayoutManager(context)
+        bookmarksRecyclerView.layoutManager = LinearLayoutManager(context,
+                LinearLayoutManager.HORIZONTAL, false)
         bookmarksRecyclerView.isNestedScrollingEnabled = false
 
         bookmarksRecyclerView.setHasFixedSize(true)
         bookmarksRecyclerView.setItemViewCacheSize(20)
         bookmarksRecyclerView.isDrawingCacheEnabled = true
         bookmarksRecyclerView.drawingCacheQuality = View.DRAWING_CACHE_QUALITY_HIGH
+        val snapHelper = LinearSnapHelper()
+        snapHelper.attachToRecyclerView(bookmarksRecyclerView)
 
+        upcomingRecyclerView.layoutManager = LinearLayoutManager(context)
         upcomingRecyclerView.isNestedScrollingEnabled = false
         upcomingRecyclerView.setItemViewCacheSize(20)
         upcomingRecyclerView.isDrawingCacheEnabled = true
         upcomingRecyclerView.drawingCacheQuality = View.DRAWING_CACHE_QUALITY_HIGH
 
         appDB = AppDB.getInstance(context!!)
-       val upcomingAdapter = UpcomingAdapter(context!!)
+        val upcomingAdapter = UpcomingAdapter(context!!)
         val bookmarksAdapter = BookmarksAdapter(context!!)
         upcomingRecyclerView.adapter = upcomingAdapter
         bookmarksRecyclerView.adapter = bookmarksAdapter
 
         upcomingAdapter.addEvents(appDB.getAllEvents()
-                .filter { (it.timestamp - 5 * 60 * 60 - 30 * 60 ) < System.currentTimeMillis() / 1000L }
+                .filter { ((it.timestamp - 5 * 60 * 60 - 30 * 60) < System.currentTimeMillis() / 1000L)
+                        .and(it.timestamp != 2L) }
                 .sortedBy { it.timestamp }
                 .subList(0, 10))
-
-
 
         if (appDB.getBookmarkedEvents().isEmpty()) {
             bookmarksRecyclerView.visibility = View.GONE
